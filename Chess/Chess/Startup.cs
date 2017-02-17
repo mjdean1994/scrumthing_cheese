7a8dd3d264d428f004fdfa0fda1c96d6ef657867
Dean <matthew.dean@champlain.edu>
2017-02-17 11:28:40 -0500

    Project setup

---
+++ Chess/Chess/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chess.Startup))]
+namespace Chess
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
