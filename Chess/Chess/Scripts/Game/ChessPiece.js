5bcb53715d5e2e67c2178eeaa4f263d87579bbdb
JackHouk <jackhouk@hotmail.com>
2017-02-24 09:26:22 -0500

    Implemented movement logic for pieces.
    Pieces can now only make moves that fit their movement rules. They can
    still move through other pieces.

--- Chess/Chess/Scripts/Game/ChessPiece.js
+++ Chess/Chess/Scripts/Game/ChessPiece.js
@@ -33,5 +33,82 @@ function ChessPiece(pieceType, team, x, y)
             return this.pieceType.spriteBlack;
         return null;
     }
+
+    //check if move is valid
+    this.validMove = function (targetX, targetY) {
+        var x = this.x;
+        var y = this.y;
+        switch(this.pieceType.name){
+            case "Pawn":
+                if (this.team == Teams.black) {
+                    if (y == 1) {
+                        if (targetY - y <= 2 && targetY > y && targetX == x)
+                            return true;
+                        else
+                            return false;
+                    }
+                    else if (targetY - y == 1 && targetX == x)
+                        return true;
+                    else
+                        return false;
+                }
+                else if (this.team == Teams.white) {
+                    if (y == 6) {
+                        if (targetY - y >= -2 && targetY < y && targetX == x)
+                            return true;
+                        else
+                            return false;
+                    }
+                    else if (targetY - y == -1 && targetX == x)
+                        return true;
+                    else
+                        return false;
+                }
+                return true;
+                    break;
+            case "Knight":
+                if (Math.abs(targetX - x) == 2 && Math.abs(targetY - y) == 1)
+                    return true;
+                else if (Math.abs(targetX - x) == 1 && Math.abs(targetY - y) == 2)
+                    return true;
+                else
+                    return false;
+                break;
+            case "Bishop":
+                if (Math.abs(targetX - x) == Math.abs(targetY - y) && targetX - x != 0)
+                    return true;
+                else
+                    return false;
+                break;
+            case "Rook":
+                if (targetX - x != 0 && targetY - y == 0)
+                    return true;
+                else if (targetX - x == 0 && targetY - y != 0)
+                    return true;
+                else
+                    return false;
+                break;
+            case "Queen":
+                if (Math.abs(targetX - x) == Math.abs(targetY - y) && targetX - x != 0)
+                    return true;
+                else if (targetX - x != 0 && targetY - y == 0)
+                    return true;
+                else if (targetX - x == 0 && targetY - y != 0)
+                    return true;
+                else
+                    return false;
+                break;
+            case "King":
+                if (Math.abs(targetX - x) <= 1 && Math.abs(targetY - y) <= 1)
+                    if (targetX - x != 0 || targetY - y != 0)
+                        return true;
+                    else
+                        return false;
+                break;
+            default:
+                return true;
+
+        }
+    }
 }
 
